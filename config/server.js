3184e642ed2c2cb809404db43e419f31254a073e
rafael sergio <rafa.sergio.02@live.com>
2017-08-16 09:14:55 -0300

    add consign em server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var consign = require('consign');
 var msg = require ('../mod_teste');
 var app = express();
 // a engine de view agora mudou, será o ejs
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
+
+	consign().include('app/routes').into(app)
+
 
 	module.exports = {
 		app,
