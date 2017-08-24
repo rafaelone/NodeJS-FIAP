0db1b156b7f060572af96d10b410c93d715e02e1
logonrm <logonrm@fiap.com.br>
2017-08-24 09:29:49 -0300

    add rota para cadastro

--- app/routes/cadastrar.js
+++ app/routes/cadastrar.js
@@ -2,4 +2,8 @@ module.exports = function(app){
 	app.get('/cadastrar', (req, res) => {
 		res.render('admin/form_add_produto');
 	});
+
+	app.post('/produtos/salvar', (req, res) => {
+		res.send('oola');
+	});
 }
