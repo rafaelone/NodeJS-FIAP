2c11802128cef0f70e5a2e7b143ec7341713681f
logonrm <logonrm@fiap.com.br>
2017-08-24 10:15:02 -0300

    arrumado form.ejs

--- config/server.js
+++ config/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
 var msg = require ('../mod_teste');
 var app = express();
 // a engine de view agora mudou, será o ejs
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
+
+	app.use(bodyParser.urlencoded({extended: true}));
 
 	consign()
 	.include('app/routes')
