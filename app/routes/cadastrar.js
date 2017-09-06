8a5c1f48f2038bd3b15b2f480602b1a9151f9de4
logonrm <logonrm@fiap.com.br>
2017-09-06 09:34:30 -0300

    add javascript e validator

--- app/routes/cadastrar.js
+++ app/routes/cadastrar.js
@@ -1,11 +1,22 @@
 module.exports = function(app){
 	app.get('/cadastrar', (req, res) => {
-		res.render('admin/form_add_produto');
+		res.render('admin/form_add_produto', {validacao: {}, produto:{} });
 	});
 
 	app.post('/produtos/salvar', (req, res) => {
 		var produto = req.body;
-	
+		
+		req.assert('nome', 'Nome é obrigatorio').notEmpty();
+		req.assert('descricao', 'Descrição é obigatoria').notEmpty();
+
+		var erros = req.validationErrors();
+
+		console.log(erros);
+		if (erros){
+			res.render('admin/form_add_produto', {validacao: erros, produto : produto});
+			return;
+		}
+
 		var connection = app.config.dbConnection();
 		var produtosModel =  new app.app.models.ProdutosDAO(connection);
 		
