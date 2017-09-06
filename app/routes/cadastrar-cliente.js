ce9d570ce4ab1062688c72f0262b993ab15954d4
logonrm <logonrm@fiap.com.br>
2017-09-06 09:33:45 -0300

    add javascript e validator

--- app/routes/cadastrar-cliente.js
+++ app/routes/cadastrar-cliente.js
@@ -1,10 +1,27 @@
 module.exports = function(app){
 	app.get('/cadastrarCliente', (req,res) => {
-		res.render('admin/form_add_cliente');
+		res.render('admin/form_add_cliente', {validacao: {}, cliente:{} });
 	});
 
 	app.post('/cliente/cadastrar', (req, res) => {
 		var cliente = req.body;
+
+		req.assert('nome', 'Nome é obrigatorio').notEmpty();
+		req.assert('sobrenome', 'sobrenome é obrigatorio').notEmpty();
+		req.assert('data_nasc', 'Data de nascimento é obrigatorio').notEmpty().isDate({format: 'DD-MM-YYYY'});
+		req.assert('cep', 'cep é obrigatorio').notEmpty();
+		req.assert('endereco', 'Endereço deve ter no minimo 10 e maximo 200 caracteres').len(10,200);
+		req.assert('cpf', 'cpf é obrigatorio').notEmpty();
+		req.assert('cpf', 'cpf deve conter 11 caracteres').len(11);
+
+		var erros = req.validationErrors();
+
+		console.log(erros);
+		if (erros){
+			res.render('admin/form_add_cliente', {validacao: erros, cliente : cliente});
+			return;
+		}
+
 		var connection = app.config.dbConnection();
 		var clienteModel = new app.app.models.ClienteDAO(connection);
 
